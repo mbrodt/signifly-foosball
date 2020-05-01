b75fba1da48e3da8ee16365f6e2dba9e9807f76e
mbrodt <madsbrodt3@gmail.com>
2020-05-01 21:07:19 +0200

    scaffold component to list all teams and players

---
+++ components/teams.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    I am the teams
+    <div v-for="team in teams" :key="team.name">
+      <h2>{{ team.name }}</h2>
+      <ul>
+        <li v-for="player in team.players" :key="player.id">
+          {{ player }}
+        </li>
+      </ul>
+    </div>
+    <add-team @addTeam="addTeam" />
+  </div>
+</template>
+
+<script>
+import addTeam from '@/components/addTeam'
+export default {
+  components: {
+    addTeam
+  },
+  props: {
+    teams: Array
+  },
+  methods: {
+    addTeam(newTeam) {
+      console.log('add team in teams', newTeam)
+      this.$emit('addTeam', newTeam)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
