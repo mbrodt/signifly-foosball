9067a7b9a0c1f0aaa99a15a95d9036c489aa1044
mbrodt <madsbrodt3@gmail.com>
2020-05-02 15:52:07 +0200

    sort teams based on points, add results and style team cards

--- components/teams.vue
+++ components/teams.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
-    I am the teams
-    <div v-for="team in teams" :key="team.name">
-      <h2>{{ team.name }}</h2>
-      <ul>
-        <li v-for="player in team.players" :key="player.id">
-          {{ player }}
-        </li>
-      </ul>
+    <h2 class="text-4xl text-blue-500">Teams</h2>
+    <div class="grid grid-cols-3 gap-8">
+      <div v-for="(team, index) in orderedTeams" :key="team.name">
+        <div
+          class="border-b-2 border-gray-500 w-8 text-center text-xl font-bold text-gray-700 leading-snug mb-2"
+        >
+          #{{ index + 1 }}
+        </div>
+        <div class="bg-white py-8 px-8 shadow-2xl">
+          <div class="flex justify-between items-center mb-2">
+            <h3 class="text-3xl font-semibold">{{ team.name }}</h3>
+            <ul class="flex space-x-4">
+              <li
+                class="text-xl flex items-center"
+                v-for="player in team.players"
+                :key="player.id"
+              >
+                <svg
+                  class="w-5 h-5 mr-1 text-gray-600"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  ></path>
+                </svg>
+                <span> {{ player }}</span>
+              </li>
+            </ul>
+          </div>
+          <results :results="getTeamResults(team).results" />
+        </div>
+      </div>
     </div>
     <add-team @addTeam="addTeam" />
   </div>
@@ -15,20 +44,58 @@
 
 <script>
 import addTeam from '@/components/addTeam'
+import results from '@/components/results'
 export default {
   components: {
-    addTeam
+    addTeam,
+    results
   },
   props: {
-    teams: Array
+    teams: Array,
+    scores: Array
+  },
+  computed: {
+    orderedTeams() {
+      const mapped = this.teams.map(team => {
+        const results = this.getTeamResults(team)
+        return results
+      })
+      const sorted = mapped.sort((team1, team2) => {
+        return team1.results.points < team2.results.points ? 1 : -1
+      })
+      return sorted
+    }
   },
   methods: {
     addTeam(newTeam) {
-      console.log('add team in teams', newTeam)
       this.$emit('addTeam', newTeam)
+    },
+    getTeamResults(team) {
+      const teamScores = this.scores.find(score => score.name === team.name)
+      const { name, ...scores } = teamScores
+
+      let wins = 0
+      let losses = 0
+
+      Object.values(scores).forEach(score => {
+        const split = score.split('-')
+        if (split[0] === '10') {
+          wins += 1
+        } else if (split[1] === '10') {
+          losses += 1
+        }
+      })
+
+      const points = wins * 3
+
+      const results = {
+        wins,
+        losses,
+        points
+      }
+
+      return { ...team, results }
     }
   }
 }
 </script>
-
-<style scoped></style>
