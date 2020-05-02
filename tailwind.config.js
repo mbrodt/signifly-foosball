76bffc354df2d86e9de82134a9cc2fe6e6a011f4
mbrodt <madsbrodt3@gmail.com>
2020-05-02 15:49:22 +0200

    extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'red-500': '#F60C1E'
+      },
+      maxWidth: {
+        '7xl': '80rem',
+        '8xl': '88rem'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
