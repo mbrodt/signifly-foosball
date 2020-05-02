0b57a185617c64923f65c1fd5cde235d84fc3305
mbrodt <madsbrodt3@gmail.com>
2020-05-02 15:49:48 +0200

    add results component

---
+++ components/results.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex justify-between items-center">
+      <div>
+        <span class="text-gray-600 text tracking-wide uppercase">Points</span>
+        <p class="font-bold text-4xl leading-none">
+          {{ results.points }}
+        </p>
+      </div>
+      <div>
+        <span class="text-gray-600 text tracking-wide uppercase">Wins</span>
+        <p class="font-bold text-4xl leading-none text-green-400">
+          {{ results.wins }}
+        </p>
+      </div>
+      <div>
+        <span class="text-gray-600 text tracking-wide uppercase">Losses</span>
+        <p class="font-bold text-4xl leading-none text-red-400">
+          {{ results.losses }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    results: Object
+  }
+}
+</script>
